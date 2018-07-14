144c56f10b0a40707836c6a3b1741ae7242cb04d
Danyllo Carlos <danyllo.rsilva@gmail.com>
2018-07-14 08:57:17 -0300

    Initial commit

---
+++ app/src/main/java/ifma/lpmobile/atv_aval01/appeventos/frm_principal.java
@@ -0,0 +1,13 @@
+package ifma.lpmobile.atv_aval01.appeventos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class frm_principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_principal);
+    }
+}
