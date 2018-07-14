77579e0c1c3ad05baed088cb1f09c5815e4c470b
Danyllo Carlos <danyllo.rsilva@gmail.com>
2018-07-14 14:20:30 -0300

    dao, model, util
    criação de 3 novos pacotes, mudanças no layout e construção do design

--- app/src/main/java/ifma/lpmobile/atv_aval01/appeventos/frm_principal.java
+++ app/src/main/java/ifma/lpmobile/atv_aval01/appeventos/frm_principal.java
@@ -2,8 +2,21 @@ package ifma.lpmobile.atv_aval01.appeventos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 
 public class frm_principal extends AppCompatActivity {
+    private EditText etTituloEvento;
+    private EditText etDescricao;
+    private Spinner spnCategoria;
+    private EditText etDataHoraInicio;
+    private EditText etDataHoraFim;
+    private ProgressBar progressBar;
+    private Button btListar;
+    private Button btSalvar;
+    private Button btSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
